2ce56729bdce672982838ff3192dbefe6282f978
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-09-10 13:55:34 +0900

    make mysql work

--- jBPM70Example_persistence_basic_mysql/src/test/java/com/sample/ProcessJPATest.java
+++ jBPM70Example_persistence_basic_mysql/src/test/java/com/sample/ProcessJPATest.java
@@ -138,7 +138,7 @@ public class ProcessJPATest {
         pds.setClassName("com.mysql.jdbc.jdbc2.optional.MysqlXADataSource");
         pds.getDriverProperties().put("user", "mysql");
         pds.getDriverProperties().put("password", "mysql");
-        pds.getDriverProperties().put("url", "jdbc:mysql://localhost:3306/testrhpam700");
+        pds.getDriverProperties().put("url", "jdbc:mysql://localhost:3306/testrhpam700?pinGlobalTxToPhysicalConnection=true");
         pds.getDriverProperties().put("driverClassName", "com.mysql.jdbc.Driver");
         pds.init();
         return pds;
